993365b9ab6256bc513250f8ca615e61f96c8d55
ijlion <ijlion@protonmail.com>
2023-03-10 16:12:23 +0400

    Add colmena pkg as nix shell

--- flake.nix
+++ flake.nix
@@ -1,30 +1,40 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, ... }: {
-    colmena = {
-      meta = {
-        nixpkgs = import nixpkgs {
-          system = "x86_64-linux";
-          overlays = [ ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShell = self.devShells.${system}.default;
+          devShells.default = pkgs.mkShell { buildInputs = [ pkgs.colmena ]; };
+        }) // {
+      colmena = {
+        meta = {
+          nixpkgs = import nixpkgs {
+            system = "x86_64-linux";
+            overlays = [ ];
+          };
         };
-      };
 
-      cild = {
-        deployment = {
-          targetHost = "88.198.7.144";
-          targetUser = "root";
-          buildOnTarget = true;
+        cild = {
+          deployment = {
+            targetHost = "88.198.7.144";
+            targetUser = "root";
+            buildOnTarget = true;
+          };
+
+          imports = [
+            ./common
+            ./services/directus
+            ./hosts/cild/configuration.nix
+          ];
         };
 
-        imports = [
-          ./common
-          ./hosts/cild/configuration.nix
-        ];
       };
-
     };
-  };
 }
